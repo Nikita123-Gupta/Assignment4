12363134bccaa73ef367631df031aaf4a27d37f1
Nikita_Gupta <nikita90139747@gmail.com>
2020-07-14 14:16:30 +0530

    Added productview , productadd and product.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
